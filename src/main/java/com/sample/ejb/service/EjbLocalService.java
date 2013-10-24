54e59c3374af6b2edc8c186200262fe26225a2b5
unknown <CP522052@CD138287.optus.com.au>
2013-10-24 15:35:23 +1100

    Added sample method for EJB local

--- src/main/java/com/sample/ejb/service/EjbLocalService.java
+++ src/main/java/com/sample/ejb/service/EjbLocalService.java
@@ -1,5 +1,16 @@
 package com.sample.ejb.service;
 
+import javax.ejb.Local;
+
+@Local
 public interface EjbLocalService {
+
+	/**
+	 * Check dummy user name
+	 * 
+	 * @param userName
+	 *            User name to be check
+	 * */
+	void checkUser(String userName);
 
 }
